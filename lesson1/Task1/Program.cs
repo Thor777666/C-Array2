ff95a6335634f03e04c240a56841f8b9483002fe
Thor777666 <fel777666@gmail.com>
2024-10-21 18:59:26 +0300

    Решил с помощью функций

--- lesson1/Task1/Program.cs
+++ lesson1/Task1/Program.cs
@@ -1,20 +1,29 @@
-﻿int[,] table = new int[3, 5];
+﻿int[,] CraeteMatrix (int line, int column){
 
-Random rnd = new Random();
+    int[,] table = new int[line, column];
 
-for (int i = 0; i < table.GetLength(0); i++)
-{
-    for (int j = 0; j < table.GetLength(1); j++)
+    Random rnd = new Random();
+
+    for (int i = 0; i < table.GetLength(0); i++)
     {
-        table[i, j] = rnd.Next(0, 11);
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            table[i, j] = rnd.Next(0, 11);
+        }
+    }
+    return table;
+}
+
+void PrintMatrix(int[,] table){
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            Console.Write(table[i, j] + " ");
+        }
+        Console.WriteLine();
     }
 }
 
-for (int i = 0; i < table.GetLength(0); i++)
-{
-    for (int j = 0; j < table.GetLength(1); j++)
-    {
-        Console.Write(table[i, j] + " ");
-    }
-    Console.WriteLine();
-}
+int[,] matrix = CraeteMatrix(4, 5);
+PrintMatrix(matrix);
